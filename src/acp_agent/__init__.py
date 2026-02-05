512ff77a429f62fc1d3fecb970c5462b17d7746f
observerw <wozluohd@gmail.com>
2026-02-05 13:18:07 +0800

    feat: add more pub

--- src/acp_agent/__init__.py
+++ src/acp_agent/__init__.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 
-from .executor import run_local
+from .executor import run_local, run_local_attached
+from .models import Registry, RegistryAgent
+from .registry import fetch_agent, fetch_registry, list_agents
 
 __all__ = [
+    "Registry",
+    "RegistryAgent",
+    "fetch_agent",
+    "fetch_registry",
+    "list_agents",
     "run_local",
+    "run_local_attached",
 ]
